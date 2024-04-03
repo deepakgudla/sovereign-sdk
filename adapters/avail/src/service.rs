fc26fb58893ec2458001d33215889d37d6828e57
deepakgudla <thedeepak1357@gmail.com>
2024-04-03 10:14:27 +0530

    avail config

--- adapters/avail/src/service.rs
+++ adapters/avail/src/service.rs
@@ -39,6 +39,14 @@ pub struct DaServiceConfig {
 pub const DEFAULT_POLLING_TIMEOUT: Duration = Duration::from_secs(60);
 pub const DEFAULT_POLLING_INTERVAL: Duration = Duration::from_secs(1);
 
+pub const LIGHT_CLIENT_URL: &str = "http://127.0.0.1:8000";
+
+pub const NODE_CLIENT_URL: &str = "http://127.0.0.1:9944";
+
+pub const SEED: &str = "bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice";
+
+
+
 #[derive(Clone)]
 pub struct DaProvider {
     pub node_client: OnlineClient<AvailConfig>,
--- examples/const-rollup-config/src/lib.rs
+++ examples/const-rollup-config/src/lib.rs
@@ -5,3 +5,11 @@ pub const ROLLUP_BATCH_NAMESPACE_RAW: [u8; 10] = [0, 0, 115, 111, 118, 45, 116, 
 
 /// The namespace used by the rollup to store aggregated ZK proofs.
 pub const ROLLUP_PROOF_NAMESPACE_RAW: [u8; 10] = [115, 111, 118, 45, 116, 101, 115, 116, 45, 112];
+
+pub const LIGHT_CLIENT_URL: &str = "http://127.0.0.1:8000";
+
+pub const NODE_CLIENT_URL: &str = "http://127.0.0.1:9944";
+
+pub const SEED: &str = "bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice";
+
+
